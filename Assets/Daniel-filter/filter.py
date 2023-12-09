854ae3d9a280f757f3e656e272d4aba5609b7114
Ein Menthos <59303591+EinMenthos@users.noreply.github.com>
2023-12-08 16:32:16 -0800

    filter + small DB
    limited DB to cities available in this software

--- Assets/Daniel-filter/filter.py
+++ Assets/Daniel-filter/filter.py
@@ -6,12 +6,15 @@ df = pd.read_csv(input_file)
 
 # Filter rows where the Country is 'US'
 filtered_df = df[df['Country'] == 'US']
+# Las Vegas, Los Angeles, Dallas, Seattle, Denver, Dallas, Chicago, Atlanta, New York City
 
 # Remove the 'Region' and 'Country' columns
 filtered_df = filtered_df.drop(['Region', 'Country'], axis=1)
 
+# filtered_test = df[filtered_df['City'] == 'Las Vegas' | df['City'] == 'Seattle' | df['City'] == 'Denver' ]
+
 # Save the filtered data to the output CSV file
 output_file = 'output.csv'
-filtered_df.to_csv(output_file, index=False)
+#filtered_df.to_csv(output_file, index=False)
 
 print(f"Filtered data saved to {output_file}")
--- Assets/Daniel_Assets/LoadData3a.cs
+++ Assets/Daniel_Assets/LoadData3a.cs
@@ -119,3 +119,4 @@ public class LoadData3a : MonoBehaviour
 
 }
 
+
